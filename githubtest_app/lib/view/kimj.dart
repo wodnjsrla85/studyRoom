109c43ff5497309b002235c02c419d415e1721e2
wodnjsrla85 <wodnjsrla85@naver.com>
2025-05-22 18:33:04 +0900

    끝
    끝

---
+++ githubtest_app/lib/view/kimj.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Kimj extends StatefulWidget {
+  const Kimj({super.key});
+
+  @override
+  State<Kimj> createState() => _KimjState();
+}
+
+class _KimjState extends State<Kimj> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+    // 순서 : 깃이랑 연결된거야 복사하고 해야해 새로운 내용이 들어올거야 그니깐 복사하고 
+  }
+}
